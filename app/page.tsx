918a221ae4a2fe783e0b3beef4d6e95ff804da7b
tejas <upadhyaytejas100@gmail.com>
2024-06-12 23:44:28 +0530

    proj setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,24 @@
-import DeployButton from "../components/DeployButton";
-import AuthButton from "../components/AuthButton";
-import { createClient } from "@/utils/supabase/server";
-import ConnectSupabaseSteps from "@/components/tutorial/ConnectSupabaseSteps";
-import SignUpUserSteps from "@/components/tutorial/SignUpUserSteps";
-import Header from "@/components/Header";
+import DeployButton from '../components/DeployButton'
+import AuthButton from '../components/AuthButton'
+import { createClient } from '@/utils/supabase/server'
+import ConnectSupabaseSteps from '@/components/tutorial/ConnectSupabaseSteps'
+import SignUpUserSteps from '@/components/tutorial/SignUpUserSteps'
+import Header from '@/components/Header'
+import Link from 'next/link'
 
 export default async function Index() {
   const canInitSupabaseClient = () => {
     // This function is just for the interactive tutorial.
     // Feel free to remove it once you have Supabase connected.
     try {
-      createClient();
-      return true;
+      createClient()
+      return true
     } catch (e) {
-      return false;
+      return false
     }
-  };
+  }
 
-  const isSupabaseConnected = canInitSupabaseClient();
+  const isSupabaseConnected = canInitSupabaseClient()
 
   return (
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
@@ -31,24 +32,34 @@ export default async function Index() {
       <div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-4xl px-3">
         <Header />
         <main className="flex-1 flex flex-col gap-6">
-          <h2 className="font-bold text-4xl mb-4">Next steps</h2>
-          {isSupabaseConnected ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
+          <h2 className="font-bold text-4xl mb-4 animate-pulse">_</h2>
+          <p>
+            <a
+              href="/login"
+              target="_blank"
+              className="font-semibold hover:underline"
+              rel="noreferrer"
+            >
+              Create an account
+            </a>{' '}
+            on Med-AI to get started
+          </p>
         </main>
       </div>
 
       <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
         <p>
-          Powered by{" "}
+          Precision Care Challenge · Developed by{' '}
           <a
-            href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
+            href="#"
             target="_blank"
             className="font-bold hover:underline"
             rel="noreferrer"
           >
-            Supabase
+            The Team
           </a>
         </p>
       </footer>
     </div>
-  );
+  )
 }
--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -1,27 +1,26 @@
-import DeployButton from "@/components/DeployButton";
-import AuthButton from "@/components/AuthButton";
-import { createClient } from "@/utils/supabase/server";
-import FetchDataSteps from "@/components/tutorial/FetchDataSteps";
-import Header from "@/components/Header";
-import { redirect } from "next/navigation";
+import DeployButton from '@/components/DeployButton'
+import AuthButton from '@/components/AuthButton'
+import { createClient } from '@/utils/supabase/server'
+import FetchDataSteps from '@/components/tutorial/FetchDataSteps'
+import Header from '@/components/Header'
+import { redirect } from 'next/navigation'
 
 export default async function ProtectedPage() {
-  const supabase = createClient();
+  const supabase = createClient()
 
   const {
     data: { user },
-  } = await supabase.auth.getUser();
+  } = await supabase.auth.getUser()
 
   if (!user) {
-    return redirect("/login");
+    return redirect('/login')
   }
 
   return (
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
       <div className="w-full">
-        <div className="py-6 font-bold bg-purple-950 text-center">
-          This is a protected page that you can only see as an authenticated
-          user
+        <div className="py-6 font-bold bg-blue-100 text-center">
+          Welcome to Med-AI
         </div>
         <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
           <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
@@ -34,24 +33,57 @@ export default async function ProtectedPage() {
       <div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-4xl px-3">
         <Header />
         <main className="flex-1 flex flex-col gap-6">
-          <h2 className="font-bold text-4xl mb-4">Next steps</h2>
-          <FetchDataSteps />
+          <h2 className="font-bold text-4xl mb-4">Upload Reports</h2>
+          {/* <FetchDataSteps /> */}
+
+          <div className="flex items-center justify-center w-full">
+            <label
+              for="dropzone-file"
+              className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
+            >
+              <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                <svg
+                  className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 20 16"
+                >
+                  <path
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
+                  />
+                </svg>
+                <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
+                  <span className="font-semibold">Click to upload</span> or drag
+                  and drop
+                </p>
+                <p className="text-xs text-gray-500 dark:text-gray-400">
+                  SVG, PNG, JPG or PDF
+                </p>
+              </div>
+              <input id="dropzone-file" type="file" className="hidden" />
+            </label>
+          </div>
         </main>
       </div>
 
       <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
         <p>
-          Powered by{" "}
+          Precision Care Challenge · Developed by{' '}
           <a
-            href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
+            href="#"
             target="_blank"
             className="font-bold hover:underline"
             rel="noreferrer"
           >
-            Supabase
+            The Team
           </a>
         </p>
       </footer>
     </div>
-  );
+  )
 }
