25d2e8da656b9d583679085d1ae52a9a6a580a89
tejas <upadhyaytejas100@gmail.com>
2024-06-19 13:05:43 +0530

    rm doc_id

--- app/analyse/page.tsx
+++ app/analyse/page.tsx
@@ -1,17 +1,17 @@
-import { createClient } from "@/utils/supabase/server";
-import Header from "@/components/Header";
-import { redirect } from "next/navigation";
-import MedForm from "@/components/MedForm";
+import { createClient } from '@/utils/supabase/server'
+import Header from '@/components/Header'
+import { redirect } from 'next/navigation'
+import MedForm from '@/components/MedForm'
 
 export default async function ProtectedPage() {
-  const supabase = createClient();
+  const supabase = createClient()
 
   const {
     data: { user },
-  } = await supabase.auth.getUser();
+  } = await supabase.auth.getUser()
   // console.log(user)
   if (!user) {
-    return redirect("/login");
+    return redirect('/login')
   }
 
   // const handleSubmit = async (formData: FormData) => {
@@ -30,11 +30,11 @@ export default async function ProtectedPage() {
         </div>
         <div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-4xl px-3">
           <p>
-            userid - {user?.id ?? ""}, mail - {user?.email ?? ""}
+            userid - {user?.id ?? ''}, mail - {user?.email ?? ''}
           </p>
-          <MedForm userId={user?.id ?? ""} docId={null} />
+          <MedForm userId={user?.id ?? ''} />
         </div>
       </div>
     </>
-  );
+  )
 }
